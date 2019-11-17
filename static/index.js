915c9956afe54ae8c1ec1d14e7f7543672e77b75
acoltelli <acoltelli@protonmail.com>
2019-11-17 14:02:12 -0800

    fix depedency, console test

--- static/index.js
+++ static/index.js
@@ -1,6 +1,7 @@
 var socket;
 $(document).ready(function(){
     socket = io.connect('http://' + document.domain + ':' + location.port + '/chat');
+    console.log("test connect")
     socket.on('connect', function() {
         socket.emit('joined', {});
     });
